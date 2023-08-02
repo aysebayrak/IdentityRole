61b8cb65ccea1607f3144909c6d3649fc4823809
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-08-02 15:51:37 +0300

    Add project files.

---
+++ Casgem_IdentityRole/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_IdentityRole.DAL
+{
+    public class Context : IdentityDbContext<AppUser,AppRole ,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-D19MH5E\\SQLEXPRESS;initial catalog=CasgemDbRole;integrated security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Casgem_IdentityRole/Models/UpdateRoleViiewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Casgem_IdentityRole.Models
+{
+    public class UpdateRoleViiewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
